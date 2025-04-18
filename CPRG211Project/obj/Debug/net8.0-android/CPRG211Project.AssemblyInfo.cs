040e45e390558d6e6ee030b102429ab66a3918b2
paphopta <paphop.ta@gmail.com>
2025-04-18 13:01:17 -0600

    Clean project

--- CPRG211Project/obj/Debug/net8.0-android/CPRG211Project.AssemblyInfo.cs
+++ CPRG211Project/obj/Debug/net8.0-android/CPRG211Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPRG211Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+59a305aec00388f040b20f929b4bcdbe79139d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPRG211Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPRG211Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
