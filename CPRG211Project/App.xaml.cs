a05b3d06c6db58ea852e337f6bcc85262bcef3d9
paphopta <paphop.ta@gmail.com>
2025-04-18 12:44:13 -0600

    Initialize project

---
+++ CPRG211Project/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CPRG211Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ CPRG211Project/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CPRG211Project;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
