7e6d16a17de015c846e185f2dcebe84b916c8372
paphopta <paphop.ta@gmail.com>
2025-04-19 19:12:27 -0600

    Test2

---
+++ MechanicalShop/Data/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicalShop.Data
+{
+    public class Customer
+    {
+        public string? CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public Customer(string customerId, string firstName, string lastName, string phone, string email)
+        {
+            this.CustomerId = customerId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+            this.Email = email;
+        }
+    }
+}
