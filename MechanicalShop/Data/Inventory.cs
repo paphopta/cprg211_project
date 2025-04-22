ef45922fadad30a8948a06f19c603d63bdbe23d3
Andrew Le <95255393+Andr3w33@users.noreply.github.com>
2025-04-22 13:58:16 -0600

    Using Paphop code (CustomerPage.razor and InventoryPage.razor) as the skeleton code
    Changes made:
    + Add exceptions in C# section
    + Add error messages after catching an exception
    + Add success message after all valid input entered
    + Add JavaScript: When all input fields are correctly entered, the input page closes.

--- MechanicalShop/Data/Inventory.cs
+++ MechanicalShop/Data/Inventory.cs
@@ -8,5 +8,20 @@ namespace MechanicalShop.Data
 {
     public class Inventory
     {
+        public string? PartId { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? PartName { get; set; }
+        public double UnitPrice { get; set; }
+        public int InventoryQuantity { get; set; }
+        public Inventory(string partId, string brand, string model, string partName, double unitPrice, int inventoryQuantity)
+        {
+            this.PartId = partId;
+            this.Brand = brand;
+            this.Model = model;
+            this.PartName = partName;
+            this.UnitPrice = unitPrice;
+            this.InventoryQuantity = inventoryQuantity;
+        }
     }
 }
