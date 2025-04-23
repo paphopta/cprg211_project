def28541562994be60465787783bf8764ea521c4
paphopta <paphop.ta@gmail.com>
2025-04-23 10:59:52 -0600

    Add job order, job order detail

--- MechanicalShop/Data/Customer.cs
+++ MechanicalShop/Data/Customer.cs
@@ -8,5 +8,18 @@ namespace MechanicalShop.Data
 {
     public class Customer
     {
+        public string? CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public Customer(string customerId, string firstName, string lastName, string phone, string email)
+        {
+            this.CustomerId = customerId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+            this.Email = email;
+        }
     }
 }
